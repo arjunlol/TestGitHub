ae37b9afa85cb9562fe76be1ecc8af38f7677857
Arjun <lallarjun@gmail.com>
2016-09-23 19:59:25 -0400

    testing out commit of python file

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-#testing out github 
+# testing out github
+import csv
+
+MY_FILE = "sample_sfpd_incident_all.csv"
+
+def parse(raw_file, delimiter):
+    """Parses a raw CSV file to a JSON-line object."""
+
+    opened_file = open(raw_file)
+
+    #Read CSV file'
+    csv_data = csv.reader(opened_file, delimiter=delimiter)
+    #Build a data structure to return parsed_data
+    #Close CSV file
+
+
+    return parsed_data
+
+
+
+
